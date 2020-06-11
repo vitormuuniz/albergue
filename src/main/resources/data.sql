2a5668a01f41cad3809d7e7696bde60ca7f6531e
vitormuuniz <vitormuuniz@gmail.com>
2020-06-11 17:12:30 -0300

    change data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into address(address_name, zip_code, city, state, country) values('rua 2', '13901264', 'amparo', 'sp', 'brasil');
-INSERT INTO Customer(title, name, last_Name, birthday, address_ID, email, password) VALUES('MRS.', 'Teste', '2', DATE('2019-09-01'), 1, 'teste@email.com', '12345');
+insert into address(address_name, zip_code, city, state, country) values('rua 2', '13900000', 'Amparo', 'SP', 'Brasil');
+INSERT INTO Customer(title, name, last_Name, birthday, address_ID, email, password) VALUES('MRS.', 'Aluno', '2', DATE('2019-09-01'), 1, 'aluno@email.com', '$2a$10$ztYTBinS/LitQOno2jjwf.x7aNLRPs0iO1pIQ9ITqtNwTMybwV/MW');
