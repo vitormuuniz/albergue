7c8a302d68276944483948428bac6802d3e2d305
vitormuuniz <vitormuuniz@gmail.com>
2020-06-25 20:25:01 -0300

    adding customer, reservation and room service

--- src/main/java/br/com/albergue/controller/HostelController.java
+++ src/main/java/br/com/albergue/controller/HostelController.java
@@ -27,20 +27,26 @@ import br.com.albergue.controller.dto.RoomDto;
 import br.com.albergue.controller.form.CustomerForm;
 import br.com.albergue.controller.form.ReservationForm;
 import br.com.albergue.controller.form.RoomForm;
-import br.com.albergue.service.HostelService;
+import br.com.albergue.service.CustomerService;
+import br.com.albergue.service.ReservationService;
+import br.com.albergue.service.RoomService;
 
 @RestController
 @RequestMapping("/api")
 public class HostelController {
 
 	@Autowired
-	private HostelService service;
+	private CustomerService customerService;
+	@Autowired
+	private ReservationService reservationService;
+	@Autowired
+	private RoomService roomService;
 
 	@PostMapping("/customers") 
 	public ResponseEntity<CustomerDto> registerCustomer(@RequestBody @Valid CustomerForm form,
 			UriComponentsBuilder uriBuilder) {
 		
-		return this.service.registerCustomer(form, uriBuilder);
+		return this.customerService.registerCustomer(form, uriBuilder);
 	}
 
 	@GetMapping("/customers") 
@@ -48,72 +54,72 @@ public class HostelController {
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable pagination)
 			throws URISyntaxException {
 		
-		return this.service.listAllCustomers(name, pagination);
+		return this.customerService.listAllCustomers(name, pagination);
 	}
 
 	@GetMapping("/customers/{id}")
 	public ResponseEntity<CustomerDto> listOneCustomer(@PathVariable Long id) {
 		
-		return this.service.listOneCustomer(id);
+		return this.customerService.listOneCustomer(id);
 	}
 
 	@DeleteMapping("/customers/{id}")
 	@Transactional
 	public ResponseEntity<?> deleteCustomer(@PathVariable Long id) {
 		
-		return this.service.deleteCustomer(id);
+		return this.customerService.deleteCustomer(id);
 	}
 
 	@PostMapping("/reservations") 
 	public ResponseEntity<ReservationDto> registerReservation(@RequestBody @Valid ReservationForm form,
 			UriComponentsBuilder uriBuilder) {
 		
-		return this.service.registerReservation(form, uriBuilder);
+		return this.reservationService.registerReservation(form, uriBuilder);
 	}
 
 	@GetMapping("/reservations") 
 	public ResponseEntity<List<ReservationDto>> listAllReservations(@RequestParam(required = false) String name,
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable pagination) {
 		
-		return this.service.listAllReservations(name, pagination);
+		return this.reservationService.listAllReservations(name, pagination);
 	}
 
 	@GetMapping("/reservations/{id}")
 	public ResponseEntity<ReservationDto> listOneReservation(@PathVariable Long id) {
 		
-		return this.service.listOneReservation(id);
+		return this.reservationService.listOneReservation(id);
 	}
 
 	@DeleteMapping("/reservations/{id}")
 	@Transactional
 	public ResponseEntity<?> deleteReservation(@PathVariable Long id) {
 		
-		return this.service.deleteReservation(id);
+		return this.reservationService.deleteReservation(id);
 	}
 
 	@PostMapping("/rooms") 
 	public ResponseEntity<RoomDto> registerRoom(@RequestBody @Valid RoomForm form, UriComponentsBuilder uriBuilder) {
 		
-		return this.service.registerRoom(form, uriBuilder);
+		return this.roomService.registerRoom(form, uriBuilder);
 	}
 
 	@GetMapping("/rooms") 
 	public ResponseEntity<List<RoomDto>> listAllRooms(@RequestParam(required = false) Integer number,
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable pagination) {
 
-		return this.service.listAllRooms(number, pagination);
+		return this.roomService.listAllRooms(number, pagination);
 	}
 
 	@GetMapping("/rooms/{id}")
 	public ResponseEntity<RoomDto> listOneRoom(@PathVariable Long id) {
 		
-		return this.service.listOneRoom(id);
+		return this.roomService.listOneRoom(id);
 	}
 
 	@DeleteMapping("/rooms/{id}")
 	@Transactional
 	public ResponseEntity<?> deleteRoom(@PathVariable Long id) {
 		
-		return this.service.deleteRoom(id);
+		return this.roomService.deleteRoom(id);
 	}
 }
---
+++ src/main/java/br/com/albergue/service/CustomerService.java
@@ -0,0 +1,68 @@
+package br.com.albergue.service;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.albergue.controller.dto.CustomerDto;
+import br.com.albergue.controller.form.CustomerForm;
+import br.com.albergue.domain.Customer;
+import br.com.albergue.repository.AddressRepository;
+import br.com.albergue.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@Autowired
+	private AddressRepository addressRepository;
+
+	public ResponseEntity<CustomerDto> registerCustomer(CustomerForm form, UriComponentsBuilder uriBuilder) {
+		Customer customer = form.returnCustomer(addressRepository);
+		customerRepository.save(customer);
+
+		URI uri = uriBuilder.path("/customers/{id}").buildAndExpand(customer.getId()).toUri();
+		return ResponseEntity.created(uri).body(new CustomerDto(customer));
+	}
+
+	public ResponseEntity<List<CustomerDto>> listAllCustomers(String name, Pageable pagination) {
+		List<CustomerDto> response = new ArrayList<>();
+
+		if (name == null)
+			response = CustomerDto.converter(customerRepository.findAll());
+		else
+			response = CustomerDto.converter(customerRepository.findByName(name));
+
+		if (response.isEmpty() || response == null)
+			return ResponseEntity.notFound().build();
+		else
+			return ResponseEntity.ok(response);
+	}
+
+	public ResponseEntity<CustomerDto> listOneCustomer(Long id) {
+		Optional<Customer> customer = customerRepository.findById(id);
+		if (customer.isPresent())
+			return ResponseEntity.ok(new CustomerDto(customer.get()));
+		else
+			return ResponseEntity.notFound().build();
+	}
+
+	public ResponseEntity<?> deleteCustomer(Long id) {
+		Optional<Customer> customer = customerRepository.findById(id);
+		if (customer.isPresent()) {
+			customerRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		} else
+			return ResponseEntity.notFound().build();
+	}
+	
+}
